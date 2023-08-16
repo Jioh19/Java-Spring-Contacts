4e93542d4dadf2af5b7d2574a773e7f0937e4f37
Jioh19 <jioh1982@gmail.com>
2023-08-16 14:29:17 -0400

    add apiresponse

--- src/main/java/com/ltp/contacts/web/ContactController.java
+++ src/main/java/com/ltp/contacts/web/ContactController.java
@@ -13,32 +13,49 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ltp.contacts.exception.ErrorResponse;
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Contact Controller", description = "Create and retrieve contacts")
 @RestController
 public class ContactController {
 
     @Autowired
     private ContactService contactService;
 
-    @Tag(name = "First Tag", description = "Retrieve all contacts")
+    @Operation(summary = "Retrieves contacts", description = "Provides a list of all contacts")
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of contacts", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Contact.class))))
     @GetMapping(value = "/contact/all")
     public ResponseEntity<List<Contact>> getContacts() {
         List<Contact> contacts = contactService.getContacts();
         return new ResponseEntity<>(contacts, HttpStatus.OK);
     }
 
-    @Tag(name = "Second Tag", description = "Retrieve contact by Id")
+    @Operation(summary = "Get contact by Id", description = "Returns a contact based on an ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Contact doesn't exist", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of contact", content = @Content(schema = @Schema(implementation = Contact.class))),
+    })
     @GetMapping(value = "/contact/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable String id) {
         Contact contact = contactService.getContactById(id);
         return new ResponseEntity<>(contact, HttpStatus.OK);
     }
 
-    @Tag(name = "Third Tag", description = "Create Contact")
+    @Operation(summary = "Create contact", description = "Create a contact from the provided payload")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful creation of contact"),
+            @ApiResponse(responseCode = "400", description = "Bad request: unsuccessful submission", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping(value = "/contact")
     public ResponseEntity<Contact> createContact(@Valid @RequestBody Contact contact) {
         contactService.saveContact(contact);
