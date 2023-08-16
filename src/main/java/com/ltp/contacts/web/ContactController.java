feea2485d2846a6d8453732b35d93b52a17d0323
Jioh19 <jioh1982@gmail.com>
2023-08-16 13:36:40 -0400

    add tags

--- src/main/java/com/ltp/contacts/web/ContactController.java
+++ src/main/java/com/ltp/contacts/web/ContactController.java
@@ -16,24 +16,29 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 public class ContactController {
-    
+
     @Autowired
     private ContactService contactService;
 
+    @Tag(name = "First Tag", description = "Retrieve all contacts")
     @GetMapping(value = "/contact/all")
     public ResponseEntity<List<Contact>> getContacts() {
         List<Contact> contacts = contactService.getContacts();
         return new ResponseEntity<>(contacts, HttpStatus.OK);
     }
 
+    @Tag(name = "Second Tag", description = "Retrieve contact by Id")
     @GetMapping(value = "/contact/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable String id) {
         Contact contact = contactService.getContactById(id);
         return new ResponseEntity<>(contact, HttpStatus.OK);
     }
 
+    @Tag(name = "Third Tag", description = "Create Contact")
     @PostMapping(value = "/contact")
     public ResponseEntity<Contact> createContact(@Valid @RequestBody Contact contact) {
         contactService.saveContact(contact);
